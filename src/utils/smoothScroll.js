6eef6a23de203ea5865db9d772e38e34204cedbb
shafiqswat <shafiqswat02@gmail.com>
2025-08-05 19:59:39 +0500

    build error

--- src/utils/smoothScroll.js
+++ src/utils/smoothScroll.js
@@ -2,9 +2,11 @@
 
 // utils/smoothScroll.js
 export const smoothScrollTo = (id) => {
+  if (typeof window === "undefined" || typeof document === "undefined") return;
+
   const target = document.getElementById(id);
   if (target) {
-    const yOffset = -80; // adjust for sticky header
+    const yOffset = -80;
     const y = target.getBoundingClientRect().top + window.pageYOffset + yOffset;
 
     window.scrollTo({
